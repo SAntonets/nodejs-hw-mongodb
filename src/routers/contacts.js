3262fd91a262c5e0c3bc7183029947bd63743b4b
Serhii Antonets <antonets.serhii.ua@gmail.com>
2024-06-05 19:01:44 +0300

    add validation

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 
 import { createContactController, deleteContactController, getContactByIdController, getContactsController, patchContactController } from "../controllers/contacts.js";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
+import { validateBody } from "../middlewares/validateBody.js";
+import { createContactSchema, updateContactSchema } from "../validation/contacts.js";
 
 
 const router = Router();
@@ -10,10 +12,14 @@ router.get('/contacts', ctrlWrapper(getContactsController));
 
 router.get('/contacts/:id', ctrlWrapper(getContactByIdController));
 
-router.post('/contacts', ctrlWrapper(createContactController)
+router.post('/contacts',
+    validateBody(createContactSchema),
+    ctrlWrapper(createContactController)
 )
 
-router.patch('/contacts/:id', ctrlWrapper(patchContactController))
+router.patch('/contacts/:id',
+    validateBody(updateContactSchema),
+    ctrlWrapper(patchContactController))
 
 router.delete('/contacts/:id', ctrlWrapper(deleteContactController)
 )
---
+++ src/validation/contacts.js
@@ -0,0 +1,54 @@
+import Joi from 'joi';
+
+export const createContactSchema = Joi.object({
+    name: Joi.string().min(3).max(20).required().messages({
+        'string.base': 'Name must be a type of string',
+        'string.min': 'Name must be at least 3 characters long',
+        'string.max': 'Name must be at most 20 characters long',
+        'any.required': 'Name is required',
+    }),
+    phoneNumber: Joi.string().min(3).max(20).required().messages({
+        'string.base': 'Phone number must be a type of string',
+        'string.min': 'Phone number must be at least 3 characters long',
+        'string.max': 'Phone number must be at most 20 characters long',
+        'any.required': 'Phone number is required',
+    }),
+    email: Joi.string().email().messages({
+        'string.email': 'Email must be a type of string and must be a valid email',
+    }),
+    isFavourite: Joi.boolean().messages({
+        'boolean.base': 'Is favourite must be a type of boolean',
+    }),
+    contactType: Joi.string().valid('work', 'home', 'personal').required().messages({
+        'string.base': 'Contact type must be a type of string',
+        'any.only': 'Contact type must be one of the following values: work, home, personal',
+        'any.required': 'Contact type is required',
+    }),
+})
+
+
+export const updateContactSchema = Joi.object({
+    name: Joi.string().min(3).max(20).messages({
+        'string.base': 'Name must be a type of string',
+        'string.min': 'Name must be at least 3 characters long',
+        'string.max': 'Name must be at most 20 characters long',
+        'any.required': 'Name is required',
+    }),
+    phoneNumber: Joi.string().min(3).max(20).messages({
+        'string.base': 'Phone number must be a type of string',
+        'string.min': 'Phone number must be at least 3 characters long',
+        'string.max': 'Phone number must be at most 20 characters long',
+        'any.required': 'Phone number is required',
+    }),
+    email: Joi.string().email().messages({
+        'string.email': 'Email must be a type of string and must be a valid email',
+    }),
+    isFavourite: Joi.boolean().messages({
+        'boolean.base': 'Is favourite must be a type of boolean',
+    }),
+    contactType: Joi.string().valid('work', 'home', 'personal').messages({
+        'string.base': 'Contact type must be a type of string',
+        'any.only': 'Contact type must be one of the following values: work, home, personal',
+        'any.required': 'Contact type is required',
+    }),
+})
