00f02f8c5678b3652c55b3ed62cdffdfd22e8ad0
Serhii Antonets <antonets.serhii.ua@gmail.com>
2024-06-16 23:20:08 +0300

    add shema

---
+++ src/db/models/session.js
@@ -0,0 +1,15 @@
+import { model, Schema } from 'mongoose';
+
+const sessionsSchema = new Schema(
+  {
+    userID: { type: String, required: true },
+    accessToken: { type: String, required: true },
+    refreshToken: { type: String, required: true },
+    accessTokenValidUntil: { type: Date, required: true },
+    refreshTokenValidUntil: { type: Date, required: true },
+  },
+  { timestamps: true, versionKey: false },
+);
+
+export const SessionsCollection = model('sessions', sessionsSchema);
+
